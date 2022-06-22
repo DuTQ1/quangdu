b241f47492e1835b8559093a495fc3a4a5f6a121
quangdu12 <truongquangdufc@gmail.com>
2022-06-22 22:53:43 +0700

    them file ex1.cpp

---
+++ ex1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+int a,b;
+std:: cout<< "Enter integer a: ";
+std:: cin>> a;
+std:: cout<< "Enter integer b: ";
+std:: cin>> b;
+std:: cout<<"Sum(a, b) = "<<a+b;
+std:: cout<< "Diff(a, b) = "<< a-b;
+return 0;
+}
